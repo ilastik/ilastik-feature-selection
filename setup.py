af019613451f957ad37eaf46b6fc961c692140a6
Stuart Berg <bergs@janelia.hhmi.org>
2016-03-08 12:50:28 -0500

    setup.py: Don't support non-cython builds.

--- setup.py
+++ setup.py
@@ -3,29 +3,10 @@ from setuptools import setup
 from distutils.extension import Extension
 
 from Cython.Distutils import build_ext
-try:
-    from Cython.Distutils import build_ext
-except ImportError:
-    use_cython = False
-else:
-    use_cython = True
 
-cmdclass = { }
-ext_modules = [ ]
-
-if use_cython:
-    ext_modules += [
-        Extension("feature_selection.mutual_information",
-                  [ "mutual_info/mutual_information.pyx" ],
-                  include_dirs=[numpy.get_include()]),
-    ]
-    cmdclass.update({ 'build_ext': build_ext })
-else:
-    ext_modules += [
-        Extension("feature_selection.mutual_information",
-                  [ "mutual_info/mutual_information.c" ],
-                  include_dirs=[numpy.get_include()]),
-    ]
+mutual_info_ext = Extension("feature_selection.mutual_information",
+                            [ "mutual_info/mutual_information.pyx" ],
+                            include_dirs=[numpy.get_include()])
 
 setup(name='feature_selection',
       version='0.122',
@@ -40,6 +21,6 @@ setup(name='feature_selection',
       tests_require=['nose', 'nose-cover3'],
       include_package_data=True,
       zip_safe=False,
-      cmdclass=cmdclass,
-      ext_modules=ext_modules
+      cmdclass={'build_ext': build_ext},
+      ext_modules=[mutual_info_ext]
 )
