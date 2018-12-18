4eb56032a9684e1ad3a592f813ec85cd334a9030
k-dominik <k-dominik@users.noreply.github.com>
2018-12-18 16:39:09 +0100

    regenerated expected values

--- tests/test_wrapper_selection.py
+++ tests/test_wrapper_selection.py
@@ -1,6 +1,11 @@
 """
 This file contains regression tests, this means that expected results have been
 generated using the same methods as in the tests!
+
+- 2018-12-18: re-generated expected results.
+  https://github.com/ilastik/ilastik-feature-selection/issues/1
+  Since the proposed workaround is to re-generate the expected results, this
+  was done, having sklearn 0.18 as a dependency
 """
 
 __author__ = 'fabian'
@@ -22,18 +27,15 @@ def digit_data():
 # Note: added random state in order to conform with the old tests. Maybe we
 # should remove that, have all in the same random state.
 @pytest.mark.parametrize('method, advanced_search, random_state, expected', [
-    ('BFS', False, 14271, (set([10, 13, 20, 29, 35, 37, 42, 44, 51, 53]), 1.2052499999999999)),
-    ('BFS', True, 14271, (set([18, 19, 20, 21, 29, 34, 38, 42, 43, 44, 46, 51, 54, 61]), 1.2092500000000002)),
-    ('SFS', False, 1275, (set([4, 18, 20, 21, 27, 28, 34, 42, 43, 58, 61, 62]), 1.2029999999999998)),
-    ('SFS', True, 1275, (set([4, 18, 20, 21, 27, 28, 34, 42, 43, 58, 61, 62]), 1.2029999999999998)),
+    ('BFS', False, 14271, (set([5, 20, 21, 27, 30, 34, 36, 38, 43, 44, 47]), 1.20525)),
+    ('BFS', True, 14271, (set([18, 20, 21, 27, 30, 34, 42, 43, 44, 54, 61]), 1.20925)),
+    ('SFS', False, 1275, (set([10, 13, 21, 26, 27, 28, 35, 38, 42, 44, 54]), 1.20525)),
+    ('SFS', True, 1275, (set([10, 13, 21, 26, 27, 28, 35, 38, 42, 44, 54]), 1.20525)),
     ('SBE', False, 1275, (
         set([
-            1, 3, 5, 7, 8, 10, 12, 15, 16, 20, 21, 26, 27, 29, 32, 33, 34, 36,
-            41, 42, 43, 44, 49, 52, 53, 54, 55, 56, 58, 59, 63]), 1.12425)),
-    ('SBE', True, 1275, (
-        set([
-            0, 1, 4, 5, 12, 15, 17, 20, 21, 22, 24, 26, 27, 29, 32, 33, 36, 39,
-            41, 42, 43, 44, 46, 51, 52, 54, 58, 61]), 1.141)),
+            1, 9, 11, 12, 15, 18, 21, 23, 26, 27, 31, 32, 34, 37, 39, 42, 43,
+            44, 45, 46, 48, 49, 54, 58, 60]), 1.12975)),
+    ('SBE', True, 1275, (set([20, 21, 26, 27, 34, 37, 38, 42, 43, 44, 49, 61]), 1.215)),
 ])
 def test_wrapper(digit_data, method, advanced_search, random_state, expected):
     X, Y = digit_data
@@ -57,13 +59,17 @@ def test_initial_set(digit_data):
         X, Y, eval_fct.evaluate_feature_set_size_penalty, method="SFS")
 
     a = feat_selector.run(do_advanced_search=False, initial_features=set(range(10)))
-    assert set(a[0]) == set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 18, 21, 27, 30, 33, 42, 43, 53])
-    np.assert_almost_equal(a[1], 1.1655)
+    expected_feature_set = set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 21, 22, 26, 27, 35, 38, 42, 43, 45, 53, 58])
+    expected_eval_func_value = 1.14475
+    assert set(a[0]) == expected_feature_set
+    np.testing.assert_almost_equal(a[1], expected_eval_func_value)
 
     feat_selector.change_method("BFS")
     a = feat_selector.run(do_advanced_search=False, initial_features=set(range(10)))
-    assert set(a[0]) == set([0, 1, 2, 3, 5, 9, 20, 21, 25, 27, 30, 40, 42, 43, 61])
-    np.assert_almost_equal(a[1], 1.1862499999999998)
+    expected_feature_set = set([0, 3, 4, 5, 6, 9, 21, 26, 27, 30, 42, 43, 45, 50, 51, 52, 53])
+    expected_eval_func_value = 1.19975
+    assert set(a[0]) == expected_feature_set
+    np.testing.assert_almost_equal(a[1], expected_eval_func_value)
 
 
 def test_permitted_features(digit_data):
